1854184e23e122200ab20be53b9b814f21889509
afeiship <1290657123@qq.com>
2024-01-22 19:50:53 +0800

    feat: add rmv

--- index.sh
+++ index.sh
@@ -9,7 +9,9 @@ alias g-remove='rm -rf * .* *.*';
 # rm
 alias rmf='rm -rf';
 alias rmn='rm -rf node_modules';
+alias rmv='rm -rf node_modules/.vite';
 alias rmh='rm -rf .history';
+
 
 # for macos
 alias rmds='find . -name ".DS_Store" -print -delete';
